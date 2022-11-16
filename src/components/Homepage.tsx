27e7b053d527c431ec6748def0ec75f658edeb88
Akhil365 <akhilkanagala18@gmail.com>
2022-11-15 21:37:53 -0600

    Created header for website

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,1 +1,13 @@
 import React from 'react';
+
+const Homepage = () => {
+    return (
+        <div className='Homepage'>
+            <header className='Home-header'>
+                Welcome to my website!
+            </header>
+        </div>
+    )
+}
+
+export default Homepage;
