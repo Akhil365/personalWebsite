194c8e328cdbdc0869268de1bf101526279fd4e7
Akhil365 <akhilkanagala18@gmail.com>
2022-11-17 00:04:41 -0600

    header/body components, created Aboutme page

---
+++ src/components/Aboutme.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Aboutme = () => {
+    return (
+        <div className='About-me'>
+            <header className='About-me-header'>
+                <h1>
+                    This is my about me page.
+                </h1>
+            </header>
+        </div>
+    )
+}
--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -3,18 +3,20 @@ import React from 'react';
 const Homepage = () => {
     return (
         <div className='Homepage'>
-            <h1 className='Home-header'>
-                Welcome to my website!
-            </h1>
-            <body>
+            <header className='Home-header'>
+                <h1>
+                    Welcome to my website!
+                </h1>
+            </header>
+            <body className='Personal-description'>
                 I am Akhil Kanagala. 
                 <br/>
                 I am a 3rd year Computer Science student at the University of Texas at Dallas.
                 <br/>
-
+                This is my personal website, where I will be organizing all my information into one place for visual purposes.
             </body>
         </div>
     )
-}
+};
 
 export default Homepage;
